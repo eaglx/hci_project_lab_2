01cb85f68eca3ff78d5108ffc250d66a10ac9052
eaglx <tango_delta@outlook.com>
2017-12-21 11:26:34 +0100

    Update lab03 No working

--- lab03/main.py
+++ lab03/main.py
@@ -1,7 +1,9 @@
 import os
-from scipy.io import wavfile as wav
 from pylab import *
 from scipy import *
+from scipy.io import wavfile as wav
+from scipy.signal import hamming
+import numpy as np
 
 FreqMaleFemale = [120, 232]
 HumanVoiceMax = [60, 160]
@@ -12,33 +14,18 @@ ManFalse = 0
 WomanTrue = 0
 WomanFalse = 0
 
-#od tąd wymazujemy-----------
-maleFemaleFreq = [120, 232]
-TS=3 #time for simple method
-humanVoiceMinMAx = [80,255]
-maleMinMax=[60,160]
-femaleMinMax=[180,270]
-HPSLoop=5
-#do tad-----------------------
 
-def simpleRecognition(rate, data):
-    if(checkBaseFreq(maleFemaleFreq[0], rate, data) < checkBaseFreq(maleFemaleFreq[1], rate, data)): 
-        return 1
-    return 0
-    
-def checkBaseFreq(freq, ratio, data):
-    box = int(1/freq*ratio)
-    return sum([listVariation(data[int(i*box):int((i+1)*box-1)],data[int((i+1)*box):int((i+2)*box-1)]) for i in range( max(0,int(len(data)/box/2-(TS/2*freq))), min(int(len(data)/box)-2,int(len(data)/box/2+(TS/2*freq))),1)])
-
-def listVariation(list1, list2): 
-    return sum([ abs(int(x)-int(y)) for x,y in zip(list1, list2)])
 
 if __name__ == "__main__":
     dirs = os.listdir("train")
     for wavFile in dirs:
         rate, data = wav.read("train/" + wavFile)
+        data = data.astype(float) / 2**16 # normalize to -1 to 1
+        data = data[:0] + data[:1] # sereo to mono
+        
         gender = wavFile.split('_')[1].split('.')[0]
-        found = simpleRecognition(rate, data)
+        found = HarmonicProductSpectrum(rate, data)
+        
         if(found == 1):
             found = 'M'
         else:
@@ -54,6 +41,7 @@ if __name__ == "__main__":
                 ManFalse += 1
             if(gender == 'K'):
                 WomanFalse += 1
+                
     print(ManTrue)
     print(ManFalse)
     print(WomanTrue)
