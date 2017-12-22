5d788a7353b08df2b5ce8d6668d12d6cd8ce52b9
eaglx <tango_delta@outlook.com>
2017-12-22 13:44:51 +0100

    Update lab03

--- lab03/main.py
+++ lab03/main.py
@@ -1,7 +1,9 @@
 import os
-from scipy.io import wavfile as wav
+import numpy as np
 from pylab import *
 from scipy import *
+from scipy.io import wavfile as wav
+import types
 
 TS = 3
 
@@ -23,28 +25,38 @@ def simpleRecognition(rate, data):
         return 0
 
 def checkBaseFreq(freq, ratio, data):
-    box = int(1/freq*ratio)
+    frame = int(1/freq*ratio)
     total = 0
     
-    maxRange = max(0, int(len(data) / box / 2 - (TS / 2 * freq)))
-    minRange = min(int(len(data)/box)-2, int(len(data)/box/2+(TS/2*freq)))
+    #maxRange = max(0, int(len(data) / frame / 2 - (TS / 2 * freq)))
+    maxRange =  int(len(data) / frame / 2)
+    #minRange = min(int(len(data)/frame)-2, int(len(data)/frame/2+(TS/2*freq)))
+    minRange = int(len(data)/frame)
     
     for i in range(maxRange, minRange):
-        list1 = data[int(i*box):int((i+1)*box-1)]
-        list2 = data[int((i+1)*box):int((i+2)*box-1)]
+        list1 = data[int(i*frame):int((i+1)*frame-1)]
+        list2 = data[int((i+1)*frame):int((i+2)*frame-1)]
         var = 0
         
         for x,y in zip(list1, list2):
-            var += abs(int(x)-int(y))
+            if type(x) is np.ndarray:
+                x = x[0]
+            if type(y) is np.ndarray:
+                y = y[0]
+            x = int(x) 
+            y = int(y)
+            var += abs(x-y)
+        
         total += var
     
-    return total 
+    return total
 
 if __name__ == "__main__":
     dirs = os.listdir("train")
     for wavFile in dirs:
-        print("processing: ", wavFile)
+        print("Open: ", wavFile)
         rate, data = wav.read("train/" + wavFile)
+        print("Processing: ", wavFile)
         gender = wavFile.split('_')[1].split('.')[0]
         found = simpleRecognition(rate, data)
         if(found == 1):
@@ -62,12 +74,12 @@ if __name__ == "__main__":
                 manFalse += 1
             if(gender == 'K'):
                 womanFalse += 1
-    print(manTrue)
-    print(manFalse)
-    print(womanTrue)
-    print(womanFalse)
+    print('MAN_TRUE: ',manTrue)
+    print('MAN_FALSE: ',manFalse)
+    print('WOMAN_TRUE: ',womanTrue)
+    print('WOMAN_FALSE: ',womanFalse)
     wsp = (manTrue + womanTrue) / sum(manTrue, manFalse, womanTrue, womanFalse)
-    print(wsp)
+    print('WSP: ',wsp)
         
         
         
